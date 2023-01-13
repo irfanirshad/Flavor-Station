182fd0185891cde5a164fae5e4307dafd317a185
irfanirshad <84003624+irfanirshad@users.noreply.github.com>
2023-01-13 07:56:17 +0530

    Implemented Token API

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,14 +1,9 @@
-"""Test custom Django management commands"""  # noqa
-
+"""Test custom Django management commands""" 
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
-
-
 
 @patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
@@ -27,3 +22,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
+
