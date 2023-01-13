6a079b41e2a97176b2a034ed71e183abb51ff8a9
irfanirshad <84003624+irfanirshad@users.noreply.github.com>
2023-01-14 00:42:10 +0530

    recipe detail API

---
+++ app/recipe/serializers.py
@@ -0,0 +1,22 @@
+"""Serializers for recipe APIs"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes"""
+    
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+        
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view.
+    Here RecipeDetailSerializer is an extension of base class RecipeSerializer
+    """
+    
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+        
